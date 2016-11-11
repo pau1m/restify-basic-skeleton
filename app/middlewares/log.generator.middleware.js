5b66ec9710d52f69776e16461d1c7e4e1dcf4417
Marcos <tio.makin@gmail.com>
2016-11-11 14:52:15 -0200

    feat(log): correct log name module; feat(readme): updated readme

--- app/middlewares/log.generator.middleware.js
+++ app/middlewares/log.generator.middleware.js
@@ -3,7 +3,7 @@ var uuid = require('uuid'),
     log = require('../services/log.service');
 
 function generateLog(req, res, next) {
-    log = log.child({reqId: uuid.v1(), module: 'scrooge-module'});
+    log = log.child({reqId: uuid.v1(), module: 'basic-restify-module'});
     req.log = log;
     next();
 }
